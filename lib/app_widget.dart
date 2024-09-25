87a0d75659eda3ff5ca0536bc21fc99e4c685eb0
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-09-24 21:08:00 -0300

    inicialização

---
+++ lib/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:salvando_project/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
